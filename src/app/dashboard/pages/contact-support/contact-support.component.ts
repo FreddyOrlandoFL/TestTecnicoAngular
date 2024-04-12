d5d94f1e3e644c5df5e66fd84ea9f7cb6e9d3222
FreddyOrlandoFL <freddyorlandofl@gmail.com>
2024-04-12 19:46:20 -0400

    validaciones

--- src/app/dashboard/pages/contact-support/contact-support.component.ts
+++ src/app/dashboard/pages/contact-support/contact-support.component.ts
@@ -2,16 +2,17 @@ import { UsersService } from './../../../services/users.services';
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder,FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 /**import { RouterModule } from '@angular/router';
 import { SidemenuComponent } from '../../../shared/sidemenu/sidemenu.component';
 * */
 import { HttpClient } from '@angular/common/http';
 import { delay } from 'rxjs';
-
+import { NgxUiLoaderModule } from 'ngx-ui-loader';
+import {NgxUiLoaderService} from   'ngx-ui-loader';
 @Component({
   standalone: true,
-  imports: [CommonModule,ReactiveFormsModule],
+  imports: [CommonModule,ReactiveFormsModule, NgxUiLoaderModule],
   templateUrl: './contact-support.component.html',
   styles: ``,
   selector: 'app-name-editor',
@@ -31,7 +32,7 @@ export  default class ContactSupportComponentz {
         categoria:  new FormControl(''),
         mensaje:  new FormControl('')
     });
-    enviar() {
+    enviar( ) {
       // TODO: Use EventEmitter with form value
       console.warn(this.contactForm.value);
       const headers = { 'Authorization': 'Bearer my-token', 'My-Custom-Header': 'foobar' };
@@ -52,27 +53,30 @@ export  default class ContactSupportComponentz {
       .subscribe(data=>{
         console.log(data)
       });
+      this.NgxUiLoaderService.start();
+      setTimeout(() => {
+        this.NgxUiLoaderService.stop();
+      }, 1500);
+
+      this.contactForm.reset();
     }
+
+
+    constructor(private formBuilder: FormBuilder,private NgxUiLoaderService : NgxUiLoaderService) {}
+
+    ngOnInit(): void {
+      this.contactForm = this.formBuilder.group(
+        {
+          nombre: ['', Validators.required],
+          nombre_empresa: ['', Validators.required],
+          correo: ['', [Validators.required, Validators.email]],
+          telefono: ['', Validators.required],
+          categoria: ['', Validators.required],
+          mensaje: ['', Validators.required],
+
+        }
+      );
+
+    }
+
 }
-
-  //  public UsersService = inject(UsersService);
-
-  /*  constructor(
-      private _builder: FormBuilder
-    ){
-        this.contactForm= this._builder.group({
-          id: 0,
-          nombre: ['',Validators.required],
-          nombre_empresa: ['',Validators.required],
-          correo: ['',Validators.compose([Validators.email,Validators.required])],
-          telefono:  ['',Validators.required],
-          categoria:  ['',Validators.required],
-          mensaje:  ['',Validators.required]
-        })
-        console.log(this.contactForm)
-    }
-
-    enviar(values){
-      console.log(values)
-    }*/
-
